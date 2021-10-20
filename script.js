10a3bc9cf7bb58c414d1b3426e69821adfeadbe5
Mauricio J. Monta <mauriciomontadg@gmail.com>
2021-10-20 16:12:30 -0300

    Fixing errors.
    Padding values was edited.

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#hamBtn").click(() => {
+  $("header").slideUp();
+  $(".links").delay(1000).slideDown(1000);
+});
+$("#hamBtnClose").click(() => {
+  $(".links").slideUp(1000);
+  $("header").delay(1000).slideDown(2500);
+});
+
+$("#downbtn").click(() => {
+  console.log("touched");
+});
