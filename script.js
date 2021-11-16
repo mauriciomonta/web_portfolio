75cd270fe840b16b383ed16075796c346a45bc40
Mauricio J. Monta <mauriciomontadg@gmail.com>
2021-11-16 16:46:17 -0300

    Fixed errors shown by Linters
    - Strings must use singlequote.

--- script.js
+++ script.js
@@ -51,14 +51,14 @@ $('a').click(() => {
   event.preventDefault();
 });*/
 
-document.getElementById("hamBtn").addEventListener("click", () => {
-  document.getElementById("links").classList.add("link-show");
+document.getElementById('hamBtn').addEventListener('click', () => {
+  document.getElementById('links').classList.add('link-show');
 });
 
-document.getElementById("hamBtnClose").addEventListener("click", () => {
-  document.getElementById("links").classList.remove("link-show");
+document.getElementById('hamBtnClose').addEventListener('click', () => {
+  document.getElementById('links').classList.remove('link-show');
 });
 
-document.getElementById("mobile-menu").addEventListener("click", () => {
-  document.getElementById("links").classList.remove("link-show");
+document.getElementById('mobile-menu').addEventListener('click', () => {
+  document.getElementById('links').classList.remove('link-show');
 });
