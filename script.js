dded55800cbb05081d8cad2513339133ae449ff0
Oliver Coimbra <olivercoimbra@gmail.com>
2021-11-17 12:25:33 -0400

    Add functions to pop up window

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ document.getElementById("hamBtnClose").addEventListener("click", () => {
 document.getElementById("mobile-menu").addEventListener("click", () => {
   document.getElementById("links").classList.remove("link-show");
 });
+
+document.getElementById("project-1").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.add("popup-visible");
+  setTimeout(function () {
+    document.getElementById("modal-popup").classList.add("popup-transparency");
+  }, 300);
+});
+
+document.getElementById("close-popup").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.remove("popup-transparency");
+  setTimeout(function () {
+    document.getElementById("modal-popup").classList.remove("popup-visible");
+  }, 300);
+});
