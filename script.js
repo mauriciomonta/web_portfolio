966c2b3e21bce15f5d4375279390755561d70e40
Mauricio J. Monta <mauriciomontadg@gmail.com>
2021-11-17 16:31:09 -0300

    Fixing errors shown by Linters
    Fixing some errors on script.js and style.css

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-document.getElementById("hamBtn").addEventListener("click", () => {
-  document.getElementById("links").classList.add("link-show");
+document.getElementById('hamBtn').addEventListener('click', () => {
+  document.getElementById('links').classList.add('link-show');
 });
 
-document.getElementById("hamBtnClose").addEventListener("click", () => {
-  document.getElementById("links").classList.remove("link-show");
+document.getElementById('hamBtnClose').addEventListener('click', () => {
+  document.getElementById('links').classList.remove('link-show');
 });
 
-document.getElementById("mobile-menu").addEventListener("click", () => {
-  document.getElementById("links").classList.remove("link-show");
+document.getElementById('mobile-menu').addEventListener('click', () => {
+  document.getElementById('links').classList.remove('link-show');
 });
 
-document.getElementById("project-1").addEventListener("click", () => {
-  document.getElementById("modal-popup").classList.add("popup-visible");
+document.getElementById('project-1').addEventListener('click', () => {
+  document.getElementById('modal-popup').classList.add('popup-visible');
   setTimeout(function () {
-    document.getElementById("modal-popup").classList.add("popup-transparency");
+    document.getElementById('modal-popup').classList.add('popup-transparency');
   }, 300);
 });
 
-document.getElementById("close-popup").addEventListener("click", () => {
-  document.getElementById("modal-popup").classList.remove("popup-transparency");
+document.getElementById('close-popup').addEventListener('click', () => {
+  document.getElementById('modal-popup').classList.remove('popup-transparency');
   setTimeout(function () {
-    document.getElementById("modal-popup").classList.remove("popup-visible");
+    document.getElementById('modal-popup').classList.remove('popup-visible');
   }, 300);
 });
