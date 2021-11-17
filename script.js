226d1a61bb6e8a5b165ab3a7492ba8a8f318c663
Mauricio J. Monta <mauriciomontadg@gmail.com>
2021-11-17 16:19:17 -0300

    Working on popup window
    We work on HTML, CSS, and JS.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById('hamBtn').addEventListener('click', () => {
-  document.getElementById('links').classList.add('link-show');
+document.getElementById("hamBtn").addEventListener("click", () => {
+  document.getElementById("links").classList.add("link-show");
 });
 
-document.getElementById('hamBtnClose').addEventListener('click', () => {
-  document.getElementById('links').classList.remove('link-show');
+document.getElementById("hamBtnClose").addEventListener("click", () => {
+  document.getElementById("links").classList.remove("link-show");
 });
 
-document.getElementById('mobile-menu').addEventListener('click', () => {
-  document.getElementById('links').classList.remove('link-show');
+document.getElementById("mobile-menu").addEventListener("click", () => {
+  document.getElementById("links").classList.remove("link-show");
 });
