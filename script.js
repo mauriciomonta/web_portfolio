ec7ebada797806f81803b0cac984bbff28fc9034
Mauricio J. Monta <mauriciomontadg@gmail.com>
2021-11-18 19:44:12 -0800

    Added email verification on javascript

--- script.js
+++ script.js
@@ -1,60 +1,99 @@
-document.getElementById('hamBtn').addEventListener('click', () => {
-  document.getElementById('links').classList.add('link-show');
+document.getElementById("hamBtn").addEventListener("click", () => {
+  document.getElementById("links").classList.add("link-show");
 });
 
-document.getElementById('hamBtnClose').addEventListener('click', () => {
-  document.getElementById('links').classList.remove('link-show');
+document.getElementById("hamBtnClose").addEventListener("click", () => {
+  document.getElementById("links").classList.remove("link-show");
 });
 
-document.getElementById('mobile-menu').addEventListener('click', () => {
-  document.getElementById('links').classList.remove('link-show');
+document.getElementById("mobile-menu").addEventListener("click", () => {
+  document.getElementById("links").classList.remove("link-show");
 });
 
-document.getElementById('project-1').addEventListener('click', () => {
-  document.getElementById('modal-popup').classList.add('popup-visible');
+document.getElementById("project-1").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.add("popup-visible");
   setTimeout(() => {
-    document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById("modal-popup").classList.add("popup-transparency");
   }, 300);
 });
 
-document.getElementById('project-2').addEventListener('click', () => {
-  document.getElementById('modal-popup').classList.add('popup-visible');
+document.getElementById("project-2").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.add("popup-visible");
   setTimeout(() => {
-    document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById("modal-popup").classList.add("popup-transparency");
   }, 300);
 });
 
-document.getElementById('project-3').addEventListener('click', () => {
-  document.getElementById('modal-popup').classList.add('popup-visible');
+document.getElementById("project-3").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.add("popup-visible");
   setTimeout(() => {
-    document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById("modal-popup").classList.add("popup-transparency");
   }, 300);
 });
 
-document.getElementById('project-4').addEventListener('click', () => {
-  document.getElementById('modal-popup').classList.add('popup-visible');
+document.getElementById("project-4").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.add("popup-visible");
   setTimeout(() => {
-    document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById("modal-popup").classList.add("popup-transparency");
   }, 300);
 });
 
-document.getElementById('project-5').addEventListener('click', () => {
-  document.getElementById('modal-popup').classList.add('popup-visible');
+document.getElementById("project-5").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.add("popup-visible");
   setTimeout(() => {
-    document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById("modal-popup").classList.add("popup-transparency");
   }, 300);
 });
 
-document.getElementById('project-6').addEventListener('click', () => {
-  document.getElementById('modal-popup').classList.add('popup-visible');
+document.getElementById("project-6").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.add("popup-visible");
   setTimeout(() => {
-    document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById("modal-popup").classList.add("popup-transparency");
   }, 300);
 });
 
-document.getElementById('close-popup').addEventListener('click', () => {
-  document.getElementById('modal-popup').classList.remove('popup-transparency');
+document.getElementById("close-popup").addEventListener("click", () => {
+  document.getElementById("modal-popup").classList.remove("popup-transparency");
   setTimeout(() => {
-    document.getElementById('modal-popup').classList.remove('popup-visible');
+    document.getElementById("modal-popup").classList.remove("popup-visible");
   }, 300);
 });
+
+/* CONTACT FORM VALIDATION */
+
+let mailInput = document.getElementById("email");
+let nameInput = document.getElementById("name");
+let surnameInput = document.getElementById("surname");
+let messageInput = document.getElementById("message-area");
+
+function validate(e) {
+  checkMail();
+  if (canSubmit == false) {
+    event.preventDefault();
+  } else {
+    console.log("done");
+  }
+}
+
+function checkMail() {
+  if (
+    mailInput.value == mailInput.value.toUpperCase() ||
+    mailInput.value.length < 15 ||
+    !mailInput.value.includes("@")
+  ) {
+    const input = document.getElementById("correo");
+    const error = document.createElement("p");
+    error.textContent = "The email address should be real and  in lowercase.";
+    input.appendChild(error);
+    document.getElementById("email").classList.add("error-input");
+    setTimeout(() => {
+      input.removeChild(error);
+      document.getElementById("email").classList.remove("error-input");
+    }, 5000);
+    canSubmit = false;
+  } else {
+    canSubmit = true;
+  }
+  return canSubmit;
+}
+/* CONTACT FORM VALIDATION */
